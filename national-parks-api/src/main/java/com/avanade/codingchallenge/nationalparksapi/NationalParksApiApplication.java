0416664088ca94539806c886ed2cde919d523eb7
Nathan Goodrich <nathan.goodrich@avanade.com>
2023-11-03 15:33:57 -0700

    Implementing national-parks-api folder

---
+++ national-parks-api/src/main/java/com/avanade/codingchallenge/nationalparksapi/NationalParksApiApplication.java
@@ -0,0 +1,13 @@
+package com.avanade.codingchallenge.nationalparksapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationalParksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NationalParksApiApplication.class, args);
+	}
+
+}
